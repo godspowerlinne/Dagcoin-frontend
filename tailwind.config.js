5d6f026fc7d1facdb5b1eeccd55c9e62c43bfbd0
godspowerlinne <amriccygodspea@gmail.com>
2025-02-06 13:00:33 +0100

    feat: Dagcoin - Terms Of Use Attached

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,26 @@ module.exports = {
       fontFamily: {
         figtree: ["Figtree", "serif"],
       },
+      screens: {
+        // Custom height based breakpoints
+        "h-sm": { raw: "(max-height: 640px)" },
+        "h-md": { raw: "(max-height: 909px)" },
+        "h-lg": { raw: "(min-height: 910px)" },
+      },
+      colors: {
+        bg: {
+          btn: "#2027C6",
+          blue: "#3038E5",
+          lightBtn: "#ffffff30",
+        },
+        text: {
+          fadedBlue: "#9DA3F9",
+          white: "#ffffff",
+          inactive: "#648CFF",
+          grey: "#9397B3",
+          darkBlue: "#1E2240",
+        },
+      },
     },
   },
   plugins: [],
